6d73d7ef99c370fbc62f1827c9429a9a11eced00
Dmitry Jum <xfiler@ymail.com>
2014-04-08 19:26:37 -0400

    list only linked images, learned that base64 is a string and should be saved as a link, removed logic checking nil for attachment

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @current_user = User.find_by(id: session[:user_id])
+    #@current_user = User.find_by(id: session[:user_id])
   end
 end

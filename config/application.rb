117a63aba6565f77876c12c6772b7fac3b9fea05
Dmitry Jum <xfiler@ymail.com>
2014-04-14 13:49:59 -0400

    some changes for env variables and stuff

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
+require 'rails/mongoid'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -24,5 +25,6 @@ module Imago
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
   end
 end

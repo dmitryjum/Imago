6a965773720fc2de73f0855cdc84f13bf789cee5
Dmitry Jum <xfiler@ymail.com>
2014-05-11 15:55:57 -0400

    radius slider works

--- app/assets/javascripts/radius.js
+++ app/assets/javascripts/radius.js
@@ -1,28 +1,11 @@
+var RadSpan;
 var setRadius = function(newRadius){
-  // logic below prevents us to set radius property beyond our limits
-  if(newRadius<minRad)
-      newRadius = minRad;
-    else if(newRadius>maxRad)
-      newRadius = maxRad;
     radius = newRadius;
     context.lineWidth = radius*2;
-// changing dot radius number on the pannel
-    radSpan.innerHTML = radius
-}
+};
 
-var minRad  = 2,
-    maxRad     = 100,
-    defaultRad = 10,
-    interval = 2,
-    radSpan    = document.getElementById('radval'),
-    decRad     = document.getElementById('decrad'),
-    incRad     = document.getElementById('incrad');
 
-decRad.addEventListener('click', function(){
-  setRadius(radius-interval)
-});
-incRad.addEventListener('click', function(){
-  setRadius(radius+interval)
-});
-
-setRadius(defaultRad);
+$('[data-slider]').on('change', function(){
+  radSpan = document.getElementById('sliderOutput2').value;
+  setRadius(radSpan);
+})

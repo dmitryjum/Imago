7c365bcc1ff7b0d2e3507e707a21a6d6193c7e53
Dmitry Jum <xfiler@ymail.com>
2014-06-14 02:36:59 -0400

    added more filters, grouped buttons

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -2,14 +2,17 @@ var canvas;
 var context;
 var radius;
 var originalImage;
+var pixelation;
 function canvasFunc() {  
   canvas = document.getElementById('canvas');
   context = canvas.getContext('2d');
   var invertButton = document.getElementById("invert-colors");
   var brightButton = document.getElementById("brightness");
+  var darkButton = document.getElementById("darkness");
   var greyButton = document.getElementById("greyscale");
   var copyButton = document.getElementById("copy");
   var pasteButton = document.getElementById("paste");
+  var pixelButton = document.getElementById("pixel");
 
   canvas.width = 900;
   canvas.height = 600;
@@ -80,6 +83,17 @@ function canvasFunc() {
     context.putImageData(imageData, 0, 0);
   };
 
+    Filters.darkness = function() {
+    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    for (var i = 0; i < data.length; i += 4) {
+      data[i] -= 15;
+      data[i + 1] -= 15;
+      data[i + 2] -= 15;
+    }
+    context.putImageData(imageData, 0, 0);
+  };
+
   Filters.greyscale = function() {
     var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     var data = imageData.data;
@@ -90,6 +104,36 @@ function canvasFunc() {
     context.putImageData(imageData, 0, 0);
   }
 
+  pixelation = 10;  //don't want to mess with custom value yet
+  // preset pixelation value will be ok for user for now
+  Filters.pixel = function(pixelation) {
+    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+    var data = imageData.data;
+    for (var y = 0; y < canvas.height; y += pixelation) {
+      for (var x = 0; x < canvas.width; x += pixelation) {
+        // get color components of the sample pixel
+        var red = data[((canvas.width * y) + x) * 4];
+        var green = data[((canvas.width * y) + x) * 4 + 1];
+        var blue = data[((canvas.width * y) + x) * 4 + 2];
+
+        // overwrite pixels in a square below and to
+        // the right of  the smaple pixel, whoes width and
+        // height are equal to the pixelation amount
+        for (var n = 0; n < pixelation; n++) {
+          for (var m = 0; m < pixelation; m++) {
+            if (x + m < canvas.width) {
+              data[((canvas.width * (y + n)) + (x + m)) * 4] = red;
+              data[((canvas.width * (y + n)) + (x + m)) * 4 + 1] = green;
+              data[((canvas.width * (y + n)) + (x + m)) * 4 + 2] = blue;
+            }
+          }
+        }
+      }
+    }
+    context.putImageData(imageData, 0, 0);
+    console.log("worked");
+  };
+
   Filters.copy = function(){
     originalImage = context.getImageData(0, 0, canvas.width, canvas.height);
   };
@@ -98,6 +142,8 @@ function canvasFunc() {
     context.putImageData(originalImage, 0, 0);
   };
 
+
+// events and listeners for filters
   copyButton.onclick = function(){
     Filters.copy();
   };
@@ -106,13 +152,21 @@ function canvasFunc() {
     Filters.paste();
   };
 
+  pixelButton.onclick = function(){
+    Filters.pixel(pixelation);
+  };
+
   brightButton.onclick = function(){
     Filters.brightness();
   };
 
+  darkButton.onclick = function(){
+    Filters.darkness();
+  };
+
   greyButton.onclick = function(){
     Filters.greyscale();
-  }
+  };
 
   invertButton.onclick = function(){
     Filters.invertColors();

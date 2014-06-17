63944195d29707b8f444fa615ff4f121a38b2f4b
Dmitry Jum <xfiler@ymail.com>
2014-06-17 00:56:05 -0400

    finally fixed the problem with aws hopefuly

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['IMAGO_AWS_SECRET_ACCESS_KEY']
 
   }
-  config.fog_directory = "bucket.imago"
+  config.fog_directory = "imagoedit"
   # config.fog_public     = false                                   # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
